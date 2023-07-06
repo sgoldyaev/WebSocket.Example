1d7b4a3bd0b7c3559d07e644a7b8b556396b1492
Sergey Goldyaev <sgoldyaev@gmail.com>
2023-07-06 10:49:25 +0300

    Update Event lifecycle

--- WebSocket.Example/TPLEventsRecycleStream/CloseReason.cs
+++ WebSocket.Example/TPLEventsRecycleStream/CloseReason.cs
@@ -1,8 +1,24 @@
-﻿namespace WebSocket.Example.TPLEventsRecycleStream;
+﻿using System.Net.WebSockets;
+
+namespace WebSocket.Example.TPLEventsRecycleStream;
 
 public enum CloseReason
 {
     Client,
     Server,
     Error,
+}
+
+public class CloseResult
+{
+    public CloseResult(CloseReason reason, WebSocketCloseStatus? status, string? description)
+    {
+        Reason = reason;
+        Status = status;
+        Description = description;
+    }
+
+    public CloseReason Reason { get; }
+    public WebSocketCloseStatus? Status { get; }
+    public string? Description { get; }
 }
